66ee118323f4213a99ff4fdb922da1744f04fc26
Mussadiq Ali <musadiq505@gmail.com>
2025-02-15 03:56:54 +0500

    Fix GitHub Pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
+import LoginSignup from "./Pages/LoginSignup";
 
 function App() {
   return (
@@ -13,16 +17,24 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Shop />} />
-        <Route path="/mens" element={<ShopCategory category="men" />} />
-        <Route path="/womens" element={<Shop category="womens" />} />
-        <Route path="/kids" element={<ShopCategory category="kid" />} />
-        <Route path="/product" element={<Product />}>
-          <Route path=":productId" element={<Product />} />
-        </Route>
+        <Route
+          path="/mens"
+          element={<ShopCategory banner={men_banner} category="men" />}
+        />
+        <Route
+          path="/womens"
+          element={<ShopCategory banner={women_banner} category="women" />}
+        />
+        <Route
+          path="/kids"
+          element={<ShopCategory banner={kid_banner} category="kid" />}
+        />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:productId" element={<Product />} />
         <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<loginSignup />} />
+        <Route path="/login" element={<LoginSignup />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrum from "../Components/Breadcrums/Breadcrum";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+
+  console.log("Product ID:", productId);
+  console.log("All Products:", all_product);
+
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  if (!product) {
+    return <div>⚠️ Product Not Found ⚠️</div>;
+  }
+
+  return (
+    <div>
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
